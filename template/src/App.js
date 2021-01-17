62dabea694e5c8b6394a07747629f220839b8a4b
reginaldop <reginaldoponciano@hotmail.com>
2021-01-17 18:56:42 -0300

    initial commit

---
+++ template/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex bg-gray-900 h-screen justify-center items-center">
+      <p className="text-white text-6xl">React Tailwindcss</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ template/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.jsx", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
